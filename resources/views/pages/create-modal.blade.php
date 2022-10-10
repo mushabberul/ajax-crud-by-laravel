c763eae089bceead5109e71f0faadebc11b36e0a
Sabbir Mia <dev.sabbirmia.com>
2022-10-10 21:43:33 +0600

    add modal and form

---
+++ resources/views/pages/create-modal.blade.php
@@ -0,0 +1,44 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProduct">
+    Add Product
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="addProduct" tabindex="-1" role="dialog" aria-labelledby="addProductLabel" aria-hidden="true">
+    <form action="" method="post">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addProductLabel">Add Product</h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group mb-3">
+                        <label for="product_name">Product Name</label>
+                        <input type="text" class="form-control" id="product_name" placeholder="Enter product name">
+                        @error('product_name')
+                        <div class="alert alert-danger" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="product_price">Price</label>
+                        <input type="number" name="product_price" class="form-control" id="product_price" placeholder="Product price">
+                        @error('product_price')
+                        <div class="alert alert-danger" role="alert">
+                            {{$message}}
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+    </form>
+</div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,43 +2,52 @@
 @push('style')
 
 @endpush
-<div class="col-md-6 m-auto">
-    <div class="card">
-        <div class="card-header">
-            <h2 class="card-title">Product List</h2>
-        </div>
-        <div class="card-body">
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Product Name</th>
-                    <th scope="col">Produce Price</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th>1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>
-                        <a href="#" class="btn btn-info">Edit</a>
-                        <a href="#" class="btn btn-danger">Delete</a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+
+@section('content')
+    <div class="col-md-6 m-auto">
+        <div class="card">
+            <div class="card-header">
+                <h2 class="card-title">Product List</h2>
+                @include('pages.create-modal')
+            </div>
+
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Produce Price</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <a href="#" class="btn btn-info">Edit</a>
+                            <a href="#" class="btn btn-danger">Delete</a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+            </div>
         </div>
     </div>
-</div>
+@endsection
+
+
+
 @push('script')
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script>
     $.ajaxSetup({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    }
-});
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    
 </script>
 @endpush
